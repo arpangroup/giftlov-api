a6f04ddcb38bcb9775f06e625114c4235ae87dfb
arpan jana <arpangroup1@gmail.com>
2024-02-16 10:33:29 +0530

    first commit

---
+++ src/main/java/com/arpan/giftlovapi/GiftlovApiApplication.java
@@ -0,0 +1,13 @@
+package com.arpan.giftlovapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftlovApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftlovApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arpan/giftlovapi/GiftlovApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arpan.giftlovapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftlovApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
